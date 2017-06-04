6249e3912dab1da47650719249c387c733c8f213
Brian Juul Andersen <mail@kree8tive.dk>
2017-06-04 21:12:57 +0200

    Stashing for now ...

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            //return "This is my default action...";
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1) {
+            //return "This is the Welcome action method";
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is:{numTimes}");
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,7 @@
+﻿namespace MvcMovie.Models
+{
+    public class SeedData
+    {
+        //TODO: HERTIL
+    }
+}
